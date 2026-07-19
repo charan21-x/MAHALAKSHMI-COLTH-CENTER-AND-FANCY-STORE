5e6c23396cefda66773133b624d66837d985e4a5
Gudi Charan <gudicharan73@gmail.com>
2026-07-19 21:03:34 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = "mongodb://gudicharan73:fancystore7386@ac-g2gu6od-shard-00-00.ezinw3m.mongodb.net:27017,ac-g2gu6od-shard-00-01.ezinw3m.mongodb.net:27017,ac-g2gu6od-shard-00-02.ezinw3m.mongodb.net:27017/?ssl=true&replicaSet=atlas-oicj50-shard-0&authSource=admin&appName=fancystorecluster";
 
 const app = express();
-const PORT = 5003;
 
 // Middleware
 app.use(cors());
