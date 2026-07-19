7acbf5c0077cce5827769a443c918f6d270e8a5d
Gudi Charan <gudicharan73@gmail.com>
2026-07-19 20:38:06 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -87,6 +87,8 @@ app.post("/orders", async(req, res) => {
     }
 });
 // Start Server
+const PORT = process.env.PORT || 5003;
+
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
